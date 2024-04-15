007ad694e1e42e89ec094f8e02d3dda33834bfaf
Adnan Yusuf <dnnyusuf@gmail.com>
2024-04-15 20:19:39 +0100

    testing 4.0

--- api/index.js
+++ api/index.js
@@ -20,7 +20,9 @@ app.use(express.json());
 app.use(cookieParser());
 
 // View engine
+// View engine setup
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views')); // This sets the views directory to the 'views' folder at the root of your project
 
 // Database connection
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
